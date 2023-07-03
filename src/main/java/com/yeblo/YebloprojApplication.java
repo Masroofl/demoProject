03f6da3e0cac43997d46d22be71f0452cb5ebfb4
Masroofl <masrooflone@gmail.com>
2023-07-04 00:55:14 +0530

    initial

---
+++ src/main/java/com/yeblo/YebloprojApplication.java
@@ -0,0 +1,13 @@
+package com.yeblo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YebloprojApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YebloprojApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yeblo/YebloprojApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeblo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YebloprojApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
